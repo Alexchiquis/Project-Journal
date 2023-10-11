ada50c6805c041993e8fb258df4e892cd34bcb3d
alexc <alexoa98@gmail.com>
2023-10-11 02:41:15 -0600

    Define Data Structure

---
+++ app/src/main/java/com/example/project_journal/JournalData.kt
@@ -0,0 +1,11 @@
+package com.example.project_journal
+
+class JournalData (
+    val id: Long,
+    val textContent: String,
+    val entryDate: String,
+    val moodEmoji: String,
+    val photoPath: String
+){
+//Maybe constructor
+}
